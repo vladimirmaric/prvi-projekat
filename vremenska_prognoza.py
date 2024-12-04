27aa8e1a34cf26e0dd7df8aec76be9b5c10fa12f
pqrvlad <ricma96@gmail.com>
2024-12-04 10:53:03 +0100

    dodat prvi fajl

---
+++ vremenska_prognoza.py
@@ -0,0 +1,9 @@
+temperatura = int(input("Unesite temperaturu"))
+
+if temperatura < 0:
+    print("Oprez klizavo")
+    
+if temperatura < 0:
+    print("Temperatura iznad 0")
+if temperatura > 30:
+    print("Ukljucite klima uredjaj")
