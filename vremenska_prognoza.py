8e2fa4a263b14d15ea42c1be7bf4a81d15f5be3f
pqrvlad <ricma96@gmail.com>
2024-12-05 09:59:39 +0100

    izmenjen test

--- vremenska_prognoza.py
+++ vremenska_prognoza.py
@@ -1,5 +1,6 @@
 temperatura = int(input("Unesite temperaturu"))
 test_temperatura = -1
+test_temperatura = 35
 temperatura = test_temperatura
 poruka = ""
 if temperatura < 0:
@@ -9,7 +10,9 @@ if temperatura > 0:
     poruka = "Temperatura iznad 0"
 if temperatura > 30:
     poruka = "Ukljucite klima uredjaj"
-ocekivana_poruka = "Oprez klizavo"
+
+    
+ocekivana_poruka = "Ukljucite klima uredjaj"
 if poruka == ocekivana_poruka:
     print("Case - ispod nule - Test prosao")
 
